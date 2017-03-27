d01f6c28af435861a5d030a6d9682a3fbf4c51a9
Ankush1989 <ankush.bhasin11@gmail.com>
2017-03-27 22:34:20 +0530

    Added reference of config.js to index.html

--- configs.js
+++ configs.js
@@ -1,4 +1,4 @@
-myApp.config(['$sceDelegateProvider', function($sceDelegateProvider) {
+weatherApp.config(['$sceDelegateProvider', function($sceDelegateProvider) {
     $sceDelegateProvider.resourceUrlWhitelist([
         'self',
         'http://api.openweathermap.org/**'
